69de34fc43d35fbbd650dafebe88cec3917d80cc
PetineKondet <112782780+PetineKondet@users.noreply.github.com>
2022-10-04 00:15:29 -0400

    ComposerApp

---
+++ Module_6/ComposerApp/ComposerDao.java
@@ -0,0 +1,8 @@
+/*
+    Kondet Petine (2022). CIS 505 Intermediate Java Programming. Bellevue University, all rights reserved.
+    Krasso, R. (2022). CIS 505 Intermediate Java Programming. Bellevue University, all rights reserved.
+*/
+
+public interface ComposerDao extends GenericDao<Composer, Integer> {
+    
+} // end ComposerDao interface
---
+++ Module_6/ComposerApp/GenericDao.java
@@ -0,0 +1,14 @@
+/*
+    Kondet Petine (2022). CIS 505 Intermediate Java Programming. Bellevue University, all rights reserved.
+    Krasso, R. (2022). CIS 505 Intermediate Java Programming. Bellevue University, all rights reserved.
+*/
+
+import java.util.List;
+
+public interface GenericDao<E, K> {
+
+    List<E> findAll();
+    E findBy(K key);
+    void insert(E entity);
+    
+} // end GenericDao interface
